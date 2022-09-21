211d2be1c9c4f2be93c2af7b45552b9f24d04574
Kyle Johnsen <kjohnsen@uga.edu>
2022-09-21 16:43:54 -0400

    added box spawning

---
+++ Assets/GameManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+
+    List<GameObject> boxes = new List<GameObject>();
+    int numHits = 0;
+    float minTime = 1;
+    float maxTime = 3;
+
+    [SerializeField]
+    GameObject boxToSpawn;
+    [SerializeField]
+    Transform player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(spawnBoxes());
+    }
+
+    IEnumerator spawnBoxes()
+	{
+		while (true)
+		{
+            yield return new WaitForSeconds(Random.Range(minTime, maxTime));
+            GameObject box = Instantiate(boxToSpawn);
+            boxes.Add(box);
+		}
+        yield return null;
+	}
+}
