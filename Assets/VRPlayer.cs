ea7d28b561014fc72c45a827f90d3ca98e9e5836
kjjohnsen <kjjohnsen@users.noreply.github.com>
2022-11-02 16:14:38 -0400

    added simple joystick movement

---
+++ Assets/VRPlayer.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRPlayer : MonoBehaviour
+{
+    public Transform head;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 leftJoy = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick);
+        Vector2 rightJoy = OVRInput.Get(OVRInput.Axis2D.SecondaryThumbstick);
+        Vector2 joy = (leftJoy + rightJoy) / 2.0f;
+        Vector3 movement = joy.x * head.right;
+        movement += joy.y * head.forward;
+        movement.y = 0;
+        //movement.Normalize();
+        this.transform.Translate(movement * Time.deltaTime);
+    }
+}
