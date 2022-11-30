d19ac44a0bf747bfa53483cc372729cfee6c4086
vel <velaboratory@gmail.com>
2022-11-30 17:10:27 -0500

    Tire-FootballGame

--- Assets/Scenes/Booths/Krish Nik Booth/TireMiddleController.cs
+++ Assets/Scenes/Booths/Krish Nik Booth/TireMiddleController.cs
@@ -7,24 +7,22 @@ public class TireMiddleController : MonoBehaviour
 {
     public GameObject ball;
     [SerializeField]
-    public static ParticleSystem particle;
+    private GameObject particle;
     
 
 
     void Start() {
+        
 
-        particle = GetComponent<ParticleSystem>();
-        particle.Stop();
-        var main = particle.main;
-        main.duration = 7f;
     }
 
     public void OnTriggerEnter(Collider other) {
         if (ball == other.gameObject) {
             Destroy(this.gameObject);
-            
-            particle.Play();
+
+            particle.SetActive(true);
         }
+
         
 
     }
