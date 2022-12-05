4f767a68666af12f122e71638433a8af5c55d10f
Kraken2308 <48652554+Kraken2308@users.noreply.github.com>
2022-12-05 14:49:36 -0500

    TireFootball Booth

--- Assets/Scenes/Booths/Krish Nik Booth/TireMiddleController.cs
+++ Assets/Scenes/Booths/Krish Nik Booth/TireMiddleController.cs
@@ -5,25 +5,38 @@ using UnityEngine;
 
 public class TireMiddleController : MonoBehaviour
 {
+    public GameObject tireMiddle;
     public GameObject ball;
-    [SerializeField]
-    private GameObject particle;
-    
+    public ParticleSystem particle;
+
 
 
     void Start() {
-        
 
     }
 
     public void OnTriggerEnter(Collider other) {
-        if (ball == other.gameObject) {
-            Destroy(this.gameObject);
 
-            particle.SetActive(true);
+        if (ball == other.gameObject) {
+            tireMiddle.gameObject.SetActive(false);
+
+            particle.Play();
+
+            StartCoroutine(resetGame());
         }
 
         
 
+    }
+
+    IEnumerator resetGame(){
+        Debug.Log("restarting game");
+
+        yield return new WaitForSeconds(4);
+
+        tireMiddle.gameObject.SetActive(true);
+
+        ball.transform.localPosition = new Vector3(-.5f, 0.1f, 0);
+        ball.transform.eulerAngles = new Vector3(0, 0, 0);
     }
 }
